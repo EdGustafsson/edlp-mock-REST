0daf80b279610a077db17cbf60f4971bbde83deb
Edwin Gustafsson <EdwinGustafsson@hotmail.com>
2021-12-20 11:26:27 +0100

    Added LoadData functionality

---
+++ learnpoint-mock-REST/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using learnpoint_mock_REST.Entities;
+
+namespace learnpoint_mock_REST.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<StudentsApiResponse> StudentsApiResponses { get; set; }
+        public DbSet<GroupsApiResponse> GroupsApiResponses { get; set; }
+        public DbSet<StaffMembersApiResponse> StaffMembersApiResponses { get; set; }
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
